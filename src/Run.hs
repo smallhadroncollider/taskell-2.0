ed14b9303c5d76da90827b9f27d0e50fd88ada76
Mark Wales <mark@smallhadroncollider.com>
2021-06-04 09:23:39 +0100

    feat: get widget width from context

--- src/Run.hs
+++ src/Run.hs
@@ -1,12 +1,10 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Run
     ( run
     ) where
 
 import Import
 import qualified RIO.List as L
+import qualified RIO.Seq as Seq
 
 import qualified Brick as B
 import qualified Graphics.Vty as V (defAttr)
@@ -17,14 +15,20 @@ data Name =
     Name
     deriving (Ord, Eq)
 
+textWidget' :: Text -> Int -> B.Widget Name
+textWidget' txt width =
+    case withWidth width txt of
+        Right lns -> B.vBox $ B.txt <$> lns
+        Left _ -> B.emptyWidget
+
 textWidget :: Text -> B.Widget Name
 textWidget txt =
-    case withWidth 20 txt of
-        Left _ -> B.emptyWidget
-        Right lns -> B.vBox $ B.txt <$> lns
+    B.Widget B.Greedy B.Greedy $ do
+        width <- (^. B.availWidthL) <$> B.getContext
+        B.render $ textWidget' txt width
 
-draw :: [Text] -> [B.Widget Name]
-draw s = [B.vBox $ B.padBottom (B.Pad 1) . textWidget <$> s]
+draw :: Seq Text -> [B.Widget Name]
+draw s = [B.vBox . toList $ B.padBottom (B.Pad 1) . B.hLimit 25 . textWidget <$> s]
 
 run :: RIO App ()
 run = do
@@ -37,8 +41,10 @@ run = do
                 , B.appAttrMap = const $ B.attrMap V.defAttr []
                 }
     let initialState =
-            [ "The first task on the list is to do this"
-            , "The second task here goes a little something like this"
-            , "I'm wrapping, I'm wrapping, I'm wrap-wrap-wrapping"
-            ]
+            Seq.fromList
+                [ "The first task on the list is to do this"
+                , "The second task here goes a little something like this"
+                , "I'm wrapping, I'm wrapping, I'm wrap-wrap-wrapping"
+                , "Fishy fish fishfishyfishyfishyfishfishyfishfish astronaut"
+                ]
     void . liftIO $ B.defaultMain app initialState
